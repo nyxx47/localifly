df538cb313c0320d916eb24ee0013a3629923900
krisna ahroid <ahroidlife@gmail.com>
2023-09-10 20:03:27 +0700

    init

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>Nuxt Routing set up successfully!</h1>
+    <p>Current route: {{ route.path }}</p>
+    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank"
+      >Learn more about Nuxt Routing</a
+    >
+  </div>
+</template>
+
+<style lang="scss">
+.wrapper {
+  h1 {
+    color: pink;
+  }
+}
+</style>
