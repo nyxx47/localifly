f99c5b5c62fecbf002a150d0e6f1ea748f2aa39e
krisna ahroid <ahroidlife@gmail.com>
2024-01-12 08:32:32 +0700

    add nuxt seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "@nuxtjs/seo"],
   app: {
     head: {
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
     },
+  },
+  // production
+  site: {
+    url: "https://localifly.com",
+    name: "Localifly",
+    description:
+      "Layanan untuk setiap bisnis, organisasi, atau individu dengan berpengalaman dalam penerjemahan semua jenis dokumen.",
+    defaultLocale: "id", // not needed if you have @nuxtjs/i18n installed
+  },
+  // development
+  // site: { indexable: false },
+
+  seo: {
+    fallbackTitle: false,
   },
   devtools: {
     enabled: true,
