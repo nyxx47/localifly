b91b43b942dc9b0437530cf2106a63f153779f06
krisna ahroid <ahroidlife@gmail.com>
2024-03-19 13:31:54 +0700

    fix blog

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -13,9 +13,12 @@
         </p>
       </div>
       <div class="content">
-        <div class="thumbnail" :style="{
-          backgroundImage: 'url(' + image + ')',
-        }"></div>
+        <div
+          class="thumbnail"
+          :style="{
+            backgroundImage: 'url(' + image + ')',
+          }"
+        ></div>
         <div class="blocks mt-4 mb-56">
           <p class="text-base mt-4 txt-primary">
             Dalam era globalisasi ini, kebutuhan akan penerjemahan dokumen
@@ -23,8 +26,9 @@
             hukum, atau bahkan keperluan pribadi. Namun, menerjemahkan dokumen
             bukanlah pekerjaan yang mudah, terutama jika dokumen tersebut
             bersifat legal atau resmi. <br />
-            Inilah mengapa Anda membutuhkan <strong>jasa penerjemah tersumpah Jakarta</strong>, dan pilihan terbaik Anda
-            adalah Localifly.
+            Inilah mengapa Anda membutuhkan <strong
+              >jasa penerjemah tersumpah Jakarta</strong
+            >, dan pilihan terbaik Anda adalah Localifly.
           </p>
           <p class="text-base mt-4 txt-primary">
             Localifly bukan sekadar jasa penerjemah biasa. Kami adalah
@@ -41,13 +45,89 @@
             menawarkan layanan penerjemahan profesional yang dapat memenuhi
             semua kebutuhan Anda.
           </p>
-          <p>
-          <p class="text-base font-bold txt-primary mt-6 mb-2 leading-snug">Apa Itu Penerjemah Tersumpah?</p>
-          Penerjemah tersumpah merupakan seorang ahli bahasa yang telah melewati berbagai macam tes kualifikasi
-          penerjemahan dan berhasil lolos. Mereka telah mengikuti dan lulus Ujian Kualifikasi Penerjemah (UKP). Setelah
-          lulus kualifikasi, Himpunan Penerjemah Indonesia mengeluarkan sertifikat resmi untuk mereka. Kemudian, mereka
-          disumpah oleh pejabat yang telah ditunjuk dan dipercaya.
-
+          <p class="text-base font-bold txt-primary mt-12 mb-2 leading-snug">
+            Apa Itu Penerjemah Tersumpah?
+          </p>
+          <p class="text-base mt-4 txt-primary">
+            Penerjemah tersumpah merupakan seorang ahli bahasa yang telah
+            melewati berbagai macam tes kualifikasi penerjemahan dan berhasil
+            lolos. Mereka telah mengikuti dan lulus Ujian Kualifikasi Penerjemah
+            (UKP). Setelah lulus kualifikasi, Himpunan Penerjemah Indonesia
+            mengeluarkan sertifikat resmi untuk mereka. Kemudian, mereka
+            disumpah oleh pejabat yang telah ditunjuk dan dipercaya.
+          </p>
+          <p class="text-base mt-4 txt-primary">
+            Penerjemah tersumpah memiliki wewenang untuk mengerjakan terjemahan
+            dokumen legal atau dokumen yang diterbitkan oleh lembaga resmi.
+            Mereka bekerja secara profesional tanpa mengurangi atau menambah
+            arti sebuah teks.
+          </p>
+          <p class="text-base font-bold txt-primary mt-12 mb-2 leading-snug">
+            Mengapa Memilih Jasa Penerjemah Tersumpah Jakarta di Localifly.com?
+          </p>
+          <p class="text-base mt-4 txt-primary">
+            Localifly.com adalah penyedia jasa penerjemah tersumpah yang
+            berpengalaman dan terpercaya. Kami memiliki tim penerjemah yang
+            tersumpah dan berlisensi, yang berarti mereka telah melalui proses
+            sertifikasi yang ketat dan diakui oleh pemerintah.
+          </p>
+          <p class="text-base font-bold txt-primary mt-12 mb-2 leading-snug">
+            Keunggulan Localifly.com
+          </p>
+          <p class="text-base mt-4 txt-primary">
+            Localifly.com memiliki beberapa keunggulan umum, seperti:
+          </p>
+          <ul class="list-disc ml-4">
+            <li>
+              <p class="text-base mt-4 txt-primary">
+                Profesionalisme: Layanan profesional biasanya memiliki tim yang
+                terlatih dan berpengalaman dalam bidangnya.
+              </p>
+            </li>
+            <li>
+              <p class="text-base mt-4 txt-primary">
+                Kualitas: Kami berkomitmen untuk memberikan hasil kerja dengan
+                standar kualitas tinggi.
+              </p>
+            </li>
+            <li>
+              <p class="text-base mt-4 txt-primary">
+                Kecepatan dan Efisiensi: Layanan online seperti ini seringkali
+                mampu menyelesaikan pekerjaan dengan lebih cepat dan efisien.
+              </p>
+            </li>
+            <li>
+              <p class="text-base mt-4 txt-primary">
+                Kemudahan Akses: Sebagai platform online, Localifly.com dapat
+                Anda akses kapan saja dan dari mana saja.
+              </p>
+            </li>
+          </ul>
+          <p class="text-base font-bold txt-primary mt-12 mb-2 leading-snug">
+            Layanan Kami
+          </p>
+          <p class="text-base mt-4 txt-primary">
+            Kami menawarkan berbagai layanan penerjemahan, termasuk penerjemahan
+            dokumen hukum, medis, teknis, dan lainnya. Tim kami terdiri dari
+            penerjemah yang memiliki keahlian di berbagai bidang, sehingga kami
+            dapat menjamin akurasi dan kualitas penerjemahan kami.
+          </p>
+          <p class="text-base italic txt-primary mt-2 mb-2 leading-snug">
+            Baca juga: Jasa Penerjemah Bahasa Jakarta Timur, Pilih Localifly!
+          </p>
+          <p class="text-base font-bold txt-primary mt-12 mb-2 leading-snug">
+            Hubungi Kami
+          </p>
+          <p class="text-base mt-4 txt-primary">
+            Jadi, jika Anda membutuhkan jasa penerjemah tersumpah Jakarta,
+            jangan ragu untuk menghubungi kami di
+            <nuxt-link
+              class="text-blue-600"
+              to="https://localifly.com"
+              target="_blank"
+              >Localifly.com</nuxt-link
+            >. Kami siap membantu Anda dengan layanan penerjemahan profesional
+            yang cepat, akurat, dan terjangkau.
           </p>
         </div>
       </div>
@@ -61,14 +141,19 @@ const image =
   "https://images.unsplash.com/photo-1541746972996-4e0b0f43e02a?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
 useHead({
-  title: 'Jasa Penerjemah Tersumpah Jakarta, Bersertifikat Resmi Terbaik 2024',
+  title: "Jasa Penerjemah Tersumpah Jakarta, Bersertifikat Resmi Terbaik 2024",
   meta: [
-    { name: 'description', content: 'Jasa Penerjemah Tersumpah Jakarta | Menerima dokumen legal hokum, medis, teknis dan lainnya | Berpengalaman dan berkualitas Hub. 087777446340' },
     {
-      name: 'keywords', content: 'Jasa Penerjemah Tersumpah Jakarta'
-    }
+      name: "description",
+      content:
+        "Jasa Penerjemah Tersumpah Jakarta | Menerima dokumen legal hokum, medis, teknis dan lainnya | Berpengalaman dan berkualitas Hub. 087777446340",
+    },
+    {
+      name: "keywords",
+      content: "Jasa Penerjemah Tersumpah Jakarta",
+    },
   ],
-})
+});
 </script>
 
 <style lang="scss" scoped>
