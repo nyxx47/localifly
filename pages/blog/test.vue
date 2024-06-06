947bdac736148cb1dd26e7cdd14c0fb404c638de
Nyx47 <ahroidlife@gmail.com>
2024-06-06 15:20:17 +0700

    Create test.vue

---
+++ pages/blog/test.vue
@@ -0,0 +1,47 @@
+<template>
+    <h1>Hello pijeee!</h1>
+</template>
+
+<script setup>
+
+useHead({
+    title: "Heloo pijee",
+    meta: [
+        {
+            name: "description",
+            content:
+                "Ini sebuah kata kata",
+        },
+        {
+            property: "og:image",
+            content:
+                "https://res.cloudinary.com/productdesign/image/upload/v1717661852/Generate_11_dohqld.png",
+        },
+        {
+            property: "og:image:width",
+            content:
+                "1200",
+        },
+        {
+            property: "og:image:height",
+            content:
+                "630",
+        },
+        {
+            property: "og:type",
+            content:
+                "website",
+        },
+        {
+            property: "og:url",
+            content:
+                "https://stockbit.com/symbol/TLKM",
+        },
+        {
+            property: "og:title",
+            content:
+                "TLKM - 1Y Chart",
+        }
+    ],
+});
+</script>
